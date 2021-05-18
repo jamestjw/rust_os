7be5aa6de2f26d65f5ee3bf7d0e10d8a5ceffb0e
James Tan <james_tan97@outlook.com>
2021-05-19 02:17:03 +0800

    Build a minimal kernel that prints "Hello World!" on startup

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # rust-os
 A small operating system in Rust based on this [blog](http://os.phil-opp.com/).
+
+# Dependencies
+
+* QEMU
+
+# How to run?
+`cargo run` 
+
+This starts our kernel in QEMU.
--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,8 @@
 
 use core::panic::PanicInfo;
 
+static HELLO: &[u8] = b"Hello World!";
+
 // Panic handler should never return, we will
 // let it loop infinitely for now
 #[panic_handler]
@@ -14,5 +16,20 @@ fn panic(_info: &PanicInfo) -> ! {
 // runtime library
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // The address of the VGA buffer which we use
+    // to write to the screen.
+    let vga_buffer = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO.iter().enumerate() {
+        unsafe {
+            // Each character is an ASCII byte followed by a color byte
+
+            // Place char byte
+            *vga_buffer.offset(i as isize * 2) = byte;
+            // Place color byte - a light cyan
+            *vga_buffer.offset(i as isize * 2 + 1) = 0xb;
+        }
+    }
+
     loop {}
 }
