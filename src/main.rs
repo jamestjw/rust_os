36142233ce89adcc356187e43df6da1e9956d0f6
James Tan <james_tan97@outlook.com>
2021-05-21 01:44:18 +0800

    Build serial port device interface & test suite

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,29 @@
 #![no_std] // don't link standard library
 #![no_main] // disable Rust-level entry points, hence we won't need a main function
+#![feature(custom_test_frameworks)] // collects functions annotated with #[test_case] attribute
+#![test_runner(rust_os::test_runner)]
+// custom_test_frameworks generates a main function that calls
+// test_runner, but this function is ignored since we use the
+// #[no_main] attribute. so we reexport the function and
+// call it ourselves
+#![reexport_test_harness_main = "test_main"]
 
 use core::panic::PanicInfo;
-
-mod vga_buffer;
+use rust_os::println;
 
 // Panic handler should never return, we will
 // let it loop infinitely for now
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
     println!("{}", info);
     loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    rust_os::test_panic_handler(info);
 }
 
 // Overriding the entrypoint of the "crt0" C
@@ -18,6 +31,9 @@ fn panic(info: &PanicInfo) -> ! {
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
+
+    #[cfg(test)]
+    test_main();
 
     loop {}
 }
